a41fb637ba885e9ca7d94114c73ffaee7a7a0230
devravik <dev.ravikgupt@gmail.com>
2024-09-05 17:13:05 +0530

    Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,7 @@
                                         Log in
                                     </a>
 
+                                    
                                     @if (Route::has('register'))
                                         <a
                                             href="{{ route('register') }}"
